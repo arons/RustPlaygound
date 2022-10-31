e1a84b400d75127d0db2db3fe0deff41b85803d1
Renzo Dani <reda@logobject.ch>
2022-10-31 17:48:31 +0100

    test

--- lesson/src/l01.rs
+++ lesson/src/l01.rs
@@ -17,6 +17,8 @@ pub fn l_01() {
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
+
+            
             Ok(num) => num,
             Err(_) => continue,
         };
